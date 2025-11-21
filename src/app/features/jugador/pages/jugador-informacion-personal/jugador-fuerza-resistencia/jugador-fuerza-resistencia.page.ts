3b914bf7285896d3ee22edb9e607368782bd4493
DesarrolloPLS <desarrollo@plasalle.mx>
2025-11-21 14:04:09 -0600

    se termina la nuevaa vista de fuerza y resistencia

--- src/app/features/jugador/pages/jugador-informacion-personal/jugador-fuerza-resistencia/jugador-fuerza-resistencia.page.ts
+++ src/app/features/jugador/pages/jugador-informacion-personal/jugador-fuerza-resistencia/jugador-fuerza-resistencia.page.ts
@@ -1,7 +1,7 @@
-import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { IonInput, IonCard, IonCardSubtitle, IonCardTitle, IonCardHeader, IonCardContent, IonIcon } from '@ionic/angular/standalone';
+import { IonInput, IonCard, IonCardTitle, IonCardHeader, IonCardContent, IonIcon, IonModal, IonButton, IonHeader, IonToolbar, IonButtons, IonContent, IonText } from '@ionic/angular/standalone';
 import { LogLevel } from 'src/app/core/enums';
 import { LoggerService } from 'src/app/core/services/logger/logger.service';
 import { FormularioUtilsService } from 'src/app/shared/utils/form/formulario-utils.service';
@@ -12,7 +12,7 @@ import { SkeletonComponent } from 'src/app/shared/components/ionic/skeleton/skel
   templateUrl: './jugador-fuerza-resistencia.page.html',
   styleUrls: ['./jugador-fuerza-resistencia.page.scss'],
   standalone: true,
-  imports: [IonIcon, IonCardContent, IonCardHeader, IonCardTitle, IonCard, CommonModule, FormsModule, ReactiveFormsModule, SkeletonComponent, IonInput]
+  imports: [IonModal, IonText, IonContent, IonButtons, IonToolbar, IonHeader, IonButton, IonIcon, IonCardContent, IonCardHeader, IonCardTitle, IonCard, CommonModule, FormsModule, ReactiveFormsModule, SkeletonComponent, IonInput]
 })
 export class JugadorFuerzaResistenciaPage implements OnInit, OnDestroy {
 
@@ -21,6 +21,11 @@ export class JugadorFuerzaResistenciaPage implements OnInit, OnDestroy {
   @Input({required: true}) cargandoData: boolean = true;
 
   private readonly _contextLog = 'JugadorFuerzaResistenciaPage';
+
+  @ViewChild('modalPotencia', { static: true }) modalPotencia!: IonModal;
+  @ViewChild('modalFuerza', { static: true }) modalFuerza!: IonModal;
+  @ViewChild('modalResistencia', { static: true }) modalResistencia!: IonModal;
+//#endregion
 //#endregion
 
 //#region Constructor
@@ -51,6 +56,30 @@ export class JugadorFuerzaResistenciaPage implements OnInit, OnDestroy {
   public getErrores(campo: string, nombreMostrar:string):string | null {
     const errores = this._formularioUtils.getCampoError(this.form, campo, nombreMostrar);
     return errores;
+  }
+
+  public openModalPotencia() {
+    this.modalPotencia.present();
+  }
+
+  public closeModalPotencia() {
+    this.modalPotencia.dismiss();
+  }
+
+   public openModalFuerza() {
+    this.modalFuerza.present();
+  }
+
+  public closeModalFuerza() {
+    this.modalFuerza.dismiss();
+  }
+
+   public openModalResistencia() {
+    this.modalResistencia.present();
+  }
+
+  public closeModalResistencia() {
+    this.modalResistencia.dismiss();
   }
 //#endregion
 
